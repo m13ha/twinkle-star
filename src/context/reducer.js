7a2560e93edf98c81bedcb85a95ae933a286217d
m13ha <nwaokocham@gmail.com>
2022-10-02 23:46:40 +0100

    final

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -17,10 +17,11 @@ const reducer = (state, action) => {
   }
   if (action.type === INTRO_NEXT || action.type === INTRO_PREV) {
     const index = action.payload.index;
+    const presentLanguage = state.presentLanguage;
     return {
       ...state,
       introPresentIndex: index,
-      introPresentText: state.introText.en[index],
+      introPresentText: state.introText[presentLanguage][index],
     };
   }
   if (action.type === START_APP) {
@@ -30,18 +31,38 @@ const reducer = (state, action) => {
     };
   }
   if (action.type === OPEN_STAR_INFO) {
+    const points = [1000, 100, 50, 20, 10];
     const index = action.payload.index;
-    const activeStarInfo = state.starText.en[index];
+    const language = state.presentLanguage;
+    const activeStarInfo = state.starText[language][index];
+    const gameItemsFound = state.gameItemsFound;
+    let formerCount = gameItemsFound[index].itemCount;
+    formerCount += points[index];
+    gameItemsFound[index].itemCount = formerCount;
     return {
       ...state,
+      typeIndex: index,
       activeStarInfo,
       isModalOpened: true,
+      gameItemsFound,
     };
   }
   if (action.type === CLOSE_MODAL) {
     return {
       ...state,
       isModalOpened: false,
+    };
+  }
+  if (action.type === 'CHANGE-LANGUAGE') {
+    const presentLanguage = action.payload.language;
+    const introPresentIndex = state.introPresentIndex;
+    const introPresentText =
+      state.introText[presentLanguage][introPresentIndex];
+
+    return {
+      ...state,
+      introPresentText,
+      presentLanguage: action.payload.language,
     };
   }
   return state;
